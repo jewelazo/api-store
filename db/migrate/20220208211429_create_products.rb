0d9e0496d118b9fcd9347473823ad992e5d9d6f8
jewelazo <gpalominoe@uni.pe>
2022-02-08 21:19:31 +0000

    create models Category and Product

---
+++ db/migrate/20220208211429_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :url_image
+      t.float :price
+      t.integer :discount
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :products, :name, unique: true
+  end
+end
