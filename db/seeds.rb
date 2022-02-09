82f47886b205b687479bcc93653e9bef41e2e5e3
jewelazo <gpalominoe@uni.pe>
2022-02-09 00:31:55 +0000

    add comments to app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
 require "json"
 
-categories_data=JSON.parse(File.read("db/data/category.json"), symbolize_names: true)
+# Parse data from category.json to hash ruby with symbols as keys
+categories_data=JSON.parse(File.read("db/data/category.json"), symbolize_names: true) 
+
+# Parse data from product.json to hash ruby with symbols as keys
 products_data=JSON.parse(File.read("db/data/product.json"), symbolize_names: true)
 
 puts "Start seeding"
 
+# When  rails db:seed is executed again ,first delete all products and then delete category data
+# because model Product is dependent from model Category,make this to avoid errors
 Product.destroy_all
 Category.destroy_all
 
 puts "Start seeding categories"
 
-
+# From each element from hashes list categories_data, I create a new category
 categories_data.each do |category_data|
     new_category = Category.new(category_data)
     puts "Category not created. Errors: #{new_category.errors.full_messages}" unless new_category.save
@@ -20,6 +25,7 @@ puts "Finish seeding categories"
 
 
 puts "Start seeding products"
+# From each category stored in Category table, I select all products which belongs to that category and then is created.
 all_categories=Category.all
 all_categories.each do |category|
     category_products_data=products_data.select { |product| product[:category]==category.id}
